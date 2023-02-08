f0b7849ab2de44eff2db0ba55df2da083e0456e4
Ian Finley <ianlukefinley@gmail.com>
2023-02-08 17:26:28 -0500

    Additional Review

--- src/Content.js
+++ src/Content.js
@@ -10,11 +10,32 @@ export default function ProfileEdit () {
       setUser(userFromAPI);
     }
     loadUsers();
+    console.log(user);
   }, []);
+
+  const changeHandler = (event) => {
+    setUser({...user, [event.target.name]: event.target.value})
+  }
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const response = await fetch (
+      `https://jsonplaceholder.typicode.com/users/${user.id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify(user),
+        headers: {
+          "Content-type": "application/json;charset=UTF-8"
+        }
+      }
+    );
+    const savedData = await response.json();
+    console.log("Saved user!", savedData);
+  }
 
   if (user.id) {
     return (
-      <form name="profileEdit">
+      <form name="profileEdit" onSubmit={(submitHandler)}>
         <div>
           <label htmlFor="username">User Name:</label>
           <input
@@ -22,12 +43,14 @@ export default function ProfileEdit () {
             name="username"
             type="text"
             value={user.username}
+            onChange={changeHandler}
           />
         </div>
         <div>
           <label htmlFor="email">Email:</label>
-          <input id="email" name="email" type="email" value={user.email} />
+          <input id="email" name="email" type="email" value={user.email} onChange={changeHandler} />
         </div>
+        <button type="submit">Save</button>
       </form>
     )
   }
